ef5a961364e0e6768f90aad4dc2296b20a224064
Thien Ly <43800313+lyluongthien@users.noreply.github.com>
2024-01-23 23:30:47 +0700

    fix mt locker

--- event_internal.go
+++ event_internal.go
@@ -16,47 +16,55 @@ type singletonMutex struct {
 }
 
 func newEvent(eventName string) *eventGroup {
-	evManager.Mt.Lock()
-	defer evManager.Mt.Unlock()
+
 	if evManager.evGroups[eventName] != nil {
 		return evManager.evGroups[eventName]
 	}
+	evManager.Mt.Lock()
 	appEventInstance := &eventGroup{EventName: eventName}
 	evManager.evGroups[eventName] = appEventInstance
+	evManager.Mt.Unlock()
 	return appEventInstance
 }
 
 func (a *eventGroup) fireEvent(data interface{}) {
-	evManager.Mt.Lock()
-	defer evManager.Mt.Unlock()
 
+	var wg sync.WaitGroup
+	evManager.Mt.Lock()
 	for _, listener := range a.Listeners {
-		go func(ch chan interface{}) {
-			ch <- data
+		wg.Add(1)
+		go func(l chan interface{}) {
+			defer wg.Done()
+			l <- data
 		}(listener)
 	}
+	wg.Wait()
+	evManager.Mt.Unlock()
 
 }
 
 func (a *eventGroup) addListenerChan(listener chan interface{}) *eventGroup {
 	evManager.Mt.Lock()
-	defer evManager.Mt.Unlock()
 	a.Listeners = append(a.Listeners, listener)
+	evManager.Mt.Unlock()
 	return a
 }
 
 func (a *eventGroup) rmListeningChan(listener chan interface{}) {
-	evManager.Mt.Lock()
-	defer evManager.Mt.Unlock()
 
 	for i, l := range a.Listeners {
 		if l == listener {
+			evManager.Mt.Lock()
 			a.Listeners = append(a.Listeners[:i], a.Listeners[i+1:]...)
+			evManager.Mt.Unlock()
 			break
 		}
 	}
 
 	if len(a.Listeners) == 0 {
+		evManager.Mt.Lock()
 		evManager.evGroups[a.EventName] = nil
+		evManager.Mt.Unlock()
 	}
+
 }
