21f64149ce27ce71afcfed78f2e0fe58c195f33a
Thien Ly <43800313+lyluongthien@users.noreply.github.com>
2024-01-23 21:25:26 +0700

    📚 add basic example

---
+++ examples/basic/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lltpkg/event"
+)
+
+func main() {
+	evName := "Greeting"
+	go func() {
+		data := "World"
+		event.FireEvent(evName, data)
+	}()
+	evChan, unSub := event.EventChanel(evName)
+	defer unSub()
+	receivedData := <-evChan
+	fmt.Println("Hello,", receivedData)
+}
